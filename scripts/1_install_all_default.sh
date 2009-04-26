82c3c276869da2e337d716e0343824ca8594e282
Ryan Pavlik <ryan.pavlik@snc.edu>
2009-04-26 13:06:25 -0500

    Fix handling of confirmation of install

--- scripts/1_install_all_default.sh
+++ scripts/1_install_all_default.sh
@@ -9,19 +9,28 @@ THISSCRIPT="1_install_all_default.sh"
 # NOLOGGING="NOLOGGING"
 source ../z_config.inc
 
-pushd . > /dev/null
 
+# array of scripts to run, in order:
 SCRIPTS=("install_flashplayer_standalone.sh" "install_tbeta_1.1.sh" "install_libpoco_workaround_local.sh" "install_pymt_svn.sh")
 
-echo "Will install all sensible defaults.  Scripts to run:"
-for SCRIPT in ${SCRIPTS[@]};do
+pushd . > /dev/null
+echo
+echo "This script will install all sensible defaults for a full multitouch setup."
+echo "It will run these install scripts, in this order:"
+for SCRIPT in ${SCRIPTS[@]}; do
 	echo " - $SCRIPT"
 done
 
-read -p "To continue, type the word 'yes' no quotes, and press enter: there is no turning back. " confirm
+echo
+echo "Once the installations start, you should not cancel them."
+echo "This is your 'last chance' to cancel."
+echo "Type 'yes' and press enter to continue, type anything else"
+echo "and enter to cancel."
+echo
+read -p "Do you want to continue? " confirm
 
-[ $confirm != "yes" ] && echo "No shame in that.  Exiting" && log_append "User canceled at confirmation point" && exit 1
-
+[ "$confirm" != "yes" ] && echo "You didn't type 'yes': No shame in that.  Exiting" && log_append "User canceled at confirmation point" && exit 1
+exit
 echo
 echo "Please enter your password here so that package installation may proceed."
 echo "If each action takes less than 15 minutes (expected) this should run un-attended past this point."
