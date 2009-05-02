9c02a65ddaacf3e745a2a28244d7a192ce0fd8c7
Ryan Pavlik <ryan.pavlik@snc.edu>
2009-05-02 15:59:56 -0500

    add section to install libraries and move away tbeta bundled versions if successful

--- scripts/install_tbeta_1.1.sh
+++ scripts/install_tbeta_1.1.sh
@@ -37,13 +37,66 @@ echo
 pushd . >/dev/null
 cd $MTROOT/nuigroup
 tar xzf $MTROOT/downloads/tbeta-1.1-lin-bin.tar.gz
+mkdir $MTROOT/nuigroup/tbeta-1.1-lin-bin/tbeta/libs/old
 log_append "Unzippped $MTROOT/downloads/tbeta-1.1-lin-bin.tar.gz to $MTROOT/nuigroup/tbeta-1.1-lin-bin"
 
 echo
 echo "Now installing dependencies from Ubuntu repositories"
 echo
 
+echo " - opencv: "
+sudo aptitude -y -q --with-recommends install libcv-dev libcvaux-dev
+if [ "$(is_installed libcv1)" = "Installed" ]; then
+	echo "succeeded, moving aside bundled version in tbeta"
+	mv $MTROOT/nuigroup/tbeta-1.1-lin-bin/tbeta/libs/libcv.* $MTROOT/nuigroup/tbeta-1.1-lin-bin/tbeta/libs/old/
+	log_append "Moved aside bundled libcv.* in favor of ubuntu versions"
+	mv $MTROOT/nuigroup/tbeta-1.1-lin-bin/tbeta/libs/libcxcore.* $MTROOT/nuigroup/tbeta-1.1-lin-bin/tbeta/libs/old/
+	log_append "Moved aside bundled libcxcore.* in favor of ubuntu versions"
+else
+	echo "not successful - tbeta will use its bundled version"
+fi
 
+echo " - v4l: "
+sudo aptitude -y -q --with-recommends install libv4l-0
+if [ "$(is_installed libv4l-0)" = "Installed" ]; then
+	echo "succeeded, moving aside bundled version in tbeta"
+	mv $MTROOT/nuigroup/tbeta-1.1-lin-bin/tbeta/libs/libv4l.* $MTROOT/nuigroup/tbeta-1.1-lin-bin/tbeta/libs/old/
+	log_append "Moved aside bundled libv4l.* in favor of ubuntu versions"
+	mv $MTROOT/nuigroup/tbeta-1.1-lin-bin/tbeta/libs/libv4l2.* $MTROOT/nuigroup/tbeta-1.1-lin-bin/tbeta/libs/old/
+	log_append "Moved aside bundled libv4l2.* in favor of ubuntu versions"	
+else
+	echo "not successful - tbeta will use its bundled version"
+fi
+
+echo " - freetype: "
+sudo aptitude -y -q --with-recommends install libfreetype6-dev
+if [ "$(is_installed libfreetype6-dev)" = "Installed" ]; then
+	echo "succeeded, moving aside bundled version in tbeta"
+	mv $MTROOT/nuigroup/tbeta-1.1-lin-bin/tbeta/libs/libfreetype.* $MTROOT/nuigroup/tbeta-1.1-lin-bin/tbeta/libs/old/
+	log_append "Moved aside bundled libfreetype.* in favor of ubuntu versions"
+else
+	echo "not successful - tbeta will use its bundled version"
+fi
+
+echo " - freeimage: "
+sudo aptitude -y -q --with-recommends install libfreeimage-dev
+if [ "$(is_installed libfreeimage-dev)" = "Installed" ]; then
+	echo "succeeded, moving aside bundled version in tbeta"
+	mv $MTROOT/nuigroup/tbeta-1.1-lin-bin/tbeta/libs/libfreeimage* $MTROOT/nuigroup/tbeta-1.1-lin-bin/tbeta/libs/old/
+	log_append "Moved aside bundled libfreeimage* in favor of ubuntu versions"
+else
+	echo "not successful - tbeta will use its bundled version"
+fi
+
+echo " - swscale: "
+sudo aptitude -y -q --with-recommends install libswscale-dev
+if [ "$(is_installed libswscale6-dev)" = "Installed" ]; then
+	echo "succeeded, moving aside bundled version in tbeta"
+	mv $MTROOT/nuigroup/tbeta-1.1-lin-bin/tbeta/libs/libswscale.* $MTROOT/nuigroup/tbeta-1.1-lin-bin/tbeta/libs/old/
+	log_append "Moved aside bundled libswscale.* in favor of ubuntu versions"
+else
+	echo "not successful - tbeta will use its bundled version"
+fi
 
 echo "Setting up libpoco, the tbeta problem child..."
 sudo aptitude -y --with-recommends install libpoco-dev
